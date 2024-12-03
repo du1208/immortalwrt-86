b33e6e51270bacfb8f73f45bf0d5ba4e728d1b11
du1208 <51355588+du1208@users.noreply.github.com>
2024-12-03 09:53:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,7 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.234/g' package/base-files/files/bin/config_generate
 #sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
